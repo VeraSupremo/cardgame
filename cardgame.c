7a9dbd11f34043213c40b3c17f8b89cbf7c066ce
VeraSupremo <martiniverinirx@gmail.com>
2023-09-30 15:15:14 -0300

    Update cardgame.c
    se crea el menu el cual constara con las opciones de jugar con la ia, salir entre otras opciones

---
+++ cardgame.c
@@ -0,0 +1,60 @@
+//nombre: Martin vera, Rut:21586190-2, seccion:A, fecha: 30/09/2023//
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<windows.h>
+#include<math.h>
+
+struct jugador{
+	char nombre[200];
+	int vida[4];
+	
+	
+};
+struct cartas{
+	char nombre[200];
+	char clase[200];
+	int ataque[75];
+	int vida[75];
+	int defensa[75];
+};
+struct historial{
+	
+};
+
+
+
+
+int main(){
+	int Sw;
+	
+	//menu de inicio para empezar
+	sleep(1);
+	printf("CARGANDO JUEGO");
+	sleep(1);
+	printf("..");
+	sleep(1);
+	printf("..");
+	sleep(1);
+	printf("..");
+	sleep(1);
+	printf("............\nJUEGO CARGADO EXITOSAMENTE\n\n\n");
+	printf("BIENVENIDO A THE CLASH OF THE GUARDIANS\n");
+	sleep(1);
+	switch(Sw){
+		case 1:
+		
+		break;
+		
+		case 2:
+			
+		break;
+			
+		case 3:
+			
+		break;
+		
+	}
+	
+	return 0;
+}
